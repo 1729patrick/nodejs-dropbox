863dbd21dac66cc248710fc6c024beb2f2a3bf6e
Patrick Battisti <1729patrick@gmail.com>
2019-04-14 16:15:01 -0300

    Configura realtime

--- src/server.js
+++ src/server.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 const app = express()
+app.use(cors())
+
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+io.on('connection', socket => {
+    socket.on('connectRoom', box => {
+        socket.join(box)
+    })
+})
 
 mongoose.connect(
     'mongodb+srv://patrickbattisti:patrickbattisti@cluster0-bjbjq.mongodb.net/omnistack?retryWrites=true',
     { useNewUrlParser: true }
 );
+
+app.use((req, res, next) => {
+    req.io = io
+
+    return next()
+})
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -15,4 +32,4 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')))
 
 app.use(require('./routes'))
 
-app.listen(3000)
+server.listen(3000)
